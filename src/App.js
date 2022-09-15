4667951e0bb7e50ecfd5f9a2f3b21edbd547b20e
pralad chy <praladchy28@gmail.com>
2022-09-15 20:11:22 +0545

    creat politicsand bussiness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import Routing  from './Routing/Routing'
+import Home from './Home'
 export const App = () => {
   return (
     <>
     <BrowserRouter>
     <Routing/>
     </BrowserRouter>
+    <Home/>
     </>
   )
 }
---
+++ src/Component/Politics/Politics.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Images from '../../Images.jpg'
+const Politics = () => {
+  return (
+    <>
+      <div>politics</div>
+      <div className='politicscontainer'>
+        <div className='imagecontainer'>
+          <img src={Images} alt="imageload" />
+        </div>
+        <div className='descriptioncontainer'>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsam dolore! Architecto porro pariatur exercitationem veniam culpa ullam ad odio ab reiciendis, autem at incidunt fugit, assumenda eum consequatur vero.</div>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Politics
