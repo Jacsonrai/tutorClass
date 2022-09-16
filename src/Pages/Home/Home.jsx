2c6d021589838b82c13583632c7ef9ef67fb9038
pralad chy <praladchy28@gmail.com>
2022-09-16 18:01:21 +0545

    politics style changed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./home.css";
+import './home.css';
 import Politics from "../../Component/Politics/Politics";
 import Bussiness from "../../Component/Bussiness/Bussiness";
 import PopularPost from "../../Component/PostType/PopularPost";
@@ -8,6 +8,8 @@ const Home = () => {
     <div className="Homecontainer">
       <div className="maincontiner">
         <div className="postContainer">
+          <Politics/>
+          <Politics/>
           <PopularPost />
         </div>
       </div>
