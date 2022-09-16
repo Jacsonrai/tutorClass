1aef5da51a0031e7e15c016ab131d42ff04f3af9
unknown <kigucha1234567@gmail.com>
2022-09-16 19:15:14 +0545

    staged changed

--- src/Component/Politics/Politics.jsx
+++ src/Component/Politics/Politics.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Images from "../../Images.jpg";
-import "./Politic.css";
+import "./politic.css";
 const Politics = () => {
   return (
     <>
@@ -9,12 +9,10 @@ const Politics = () => {
           <img src={Images} alt="imageload" />
         </div>
         <div className="descriptioncontainer">
-          
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsam
-            dolore! Architecto porro pariatur exercitationem veniam culpa ullam
-            ad odio ab reiciendis, autem at incidunt fugit, assumenda eum
-            consequatur vero.
-          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsam
+          dolore! Architecto porro pariatur exercitationem veniam culpa ullam ad
+          odio ab reiciendis, autem at incidunt fugit, assumenda eum consequatur
+          vero.
         </div>
       </div>
     </>
